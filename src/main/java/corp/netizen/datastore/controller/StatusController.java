e7374d5ca4824ff177a7c5ae5704446172158e3b
Adam Bunkowski <adam.bunkowski@computaris.com>
2019-08-28 15:43:38 +0200

    added endpoints for status check

---
+++ src/main/java/corp/netizen/datastore/controller/StatusController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corp.netizen.datastore.controller;
+
+import corp.netizen.datastore.model.Mib;
+import java.util.ArrayList;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author adam.bunkowski
+ */
+@RestController
+public class StatusController {
+
+    @RequestMapping(path="/status/running/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<HttpStatus> setRunningStatus(@PathVariable("id") long id) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+	responseHeaders.set("MyResponseHeader", "inactive:" + id);
+	return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    
+    @RequestMapping(path="/status/inactive/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<HttpStatus> setInactiveStatus(@PathVariable("id") long id) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+	responseHeaders.set("MyResponseHeader", "inactive: " + id);
+	return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    
+}
